ff48e0560fc86edaf140c752815164c47bc6cc0c
k1574 <k1574@yandex.ru>
2021-01-15 01:12:53 +0500

    lua/module: added.

---
+++ lua/module/main.lua
@@ -0,0 +1,5 @@
+module = require("module")
+
+print(module.sum(5, 8))
+print(module.sub(5, 4))
+
---
+++ lua/module/module.lua
@@ -0,0 +1,22 @@
+local module = {}
+
+function module.sum(a, b)
+	return a + b
+end
+
+
+function module.sub(a, b)
+	return a - b
+end
+
+
+function module.mul(a, b)
+	return a * b
+end
+
+function module.div(a, b)
+	return a / b
+end
+
+return module
+
