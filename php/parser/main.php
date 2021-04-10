10023fc375562f66829de9fa2454a519a0757994
k1574 <k1574@yandex.ru>
2021-04-10 19:18:36 +0500

    A few new examples.

---
+++ php/parser/main.php
@@ -0,0 +1,14 @@
+<?php
+
+
+class Parser {
+	private static function getArg($a = [], $k = ""){
+		return !empty($a[])
+	}
+	public static function getPage($a = []){
+		if(!$a || empty($a["url"]) ))
+			return;
+		$url = $a["url"] ;
+	}
+}
+?>
