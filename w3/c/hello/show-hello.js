5f96f0f85a60becbd094e8f8f1fb62a181716f28
k1574 <k1574@yandex.ru>
2021-06-22 19:35:23 +0500

    web->w3, added example for Web Components.

---
+++ w3/c/hello/show-hello.js
@@ -0,0 +1,31 @@
+"use strict";
+customElements.define("show-hello", class extends HTMLElement {
+	// Most of the callbacks are described.
+
+	constructor() {
+		super();
+	}
+
+	// Element is added.
+	connectedCallback() { setTimeout(() =>{
+		const shadow = this.attachShadow({mode: "open"}) ;
+		shadow.innerHTML = `<p>
+			Hello, ${this.getAttribute("name")}.
+			${this.innerHTML}
+		</p>` ;
+	}); }
+
+	// Element is removed.
+	disconnectedCallback() {
+	}
+
+	// Array of attributes to callback on their change.
+	static get observedAttributes() {
+		return ["name"] ;
+	}
+	// Callback itself.
+	attributeChangedCallback(name, oldval, newval) {
+		console.log(`${name} was ${oldval} became ${newval}`)
+	}
+});
+
