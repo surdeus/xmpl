936b05c231ad62d5fa7a59681c3b3140eae87c0f
surdeus <surdeus@gmail.com>
2023-10-22 19:03:43 +0300

    ...

---
+++ go/gen/painkiller/pill.go
@@ -0,0 +1,14 @@
+package painkiller
+
+type Pill int
+
+const (
+	Placebo Pill = iota
+	Aspirin
+	Ibuprofen
+	Paracetamol
+	Acetaminophen = Paracetamol
+)
+
+//go:generate stringer -type=Pill -output pill-string.go
+
